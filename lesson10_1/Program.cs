7855dcbadc439e363ddae5093976c2938340b5a9
Vladimir Stepanov <grazy@list.ru>
2020-06-30 17:06:07 +0300

    создал новый проект в решении

---
+++ lesson10_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesson10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //клиент вводит пол и имя
+            
+        }
+
+        static void Step1(bool gender, string name)
+        {
+            if (gender == true)//муж - true, жун - false
+            {
+                Console.WriteLine($"Еважаемый, {name}!");
+            }
+            else
+            {
+                Console.WriteLine($"Уважаемая, {name}!");
+            }
+
+            Console.WriteLine("Приветствуем Вас на нашем сайте!");
+
+            Console.ReadLine();
+        }
+    }
+}
