68f6208fda257e5a54aee9fb29a2e788aca2dab0
Vladimir Stepanov <grazy@list.ru>
2020-06-30 23:24:08 +0300

    закомментировал 10 урок

--- Lesson10/Program.cs
+++ Lesson10/Program.cs
@@ -4,91 +4,91 @@ namespace Lesson10
 {
     class Program
     {
-        public const string ProgramName = "Календарь"; //тестовый коммит
+        //public const string ProgramName = "Календарь"; //тестовый коммит
 
-        static void Main(string[] args) // проверка связи гита и vs
-        {
-            //ввод от клиента, пол и имя
+        //static void Main(string[] args) // проверка связи гита и vs
+        //{
+        //    //ввод от клиента, пол и имя
 
-            Step1((GenderEnum)0, "Иванова Ольга");
-            //Step2(true, "Петров Иван");
+        //    Step1((GenderEnum)0, "Иванова Ольга");
+        //    //Step2(true, "Петров Иван");
 
-            StepWeek1((DayOfWeekEnum)6);
+        //    StepWeek1((DayOfWeekEnum)6);
 
-            PrintAllDays();
-            PrintAllGenders();
-        }
+        //    PrintAllDays();
+        //    PrintAllGenders();
+        //}
 
-        static void Step1(GenderEnum gender, string name)
-        {
-            if (gender == GenderEnum.Man)
-            {
-                Console.WriteLine($"Уважаемый, {name}!");
-            }
-            else
-            {
-                Console.WriteLine($"Уважаемая, {name}!");
-            }
+        //static void Step1(GenderEnum gender, string name)
+        //{
+        //    if (gender == GenderEnum.Man)
+        //    {
+        //        Console.WriteLine($"Уважаемый, {name}!");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"Уважаемая, {name}!");
+        //    }
 
-            Console.WriteLine("Приветствуем на нашем сайте!");
-        }
+        //    Console.WriteLine("Приветствуем на нашем сайте!");
+        //}
 
-        static void StepWeek1(DayOfWeekEnum dayOfWeek)
-        {
-            if(dayOfWeek == DayOfWeekEnum.Saturday || dayOfWeek == DayOfWeekEnum.Sunday) 
-            {
-                Console.WriteLine($"{DayToRussian(dayOfWeek)} - выходной день");
-            }
-            else
-            {
-                Console.WriteLine($"{DayToRussian(dayOfWeek)} - рабочий день");
-            }
-        }
+        //static void StepWeek1(DayOfWeekEnum dayOfWeek)
+        //{
+        //    if(dayOfWeek == DayOfWeekEnum.Saturday || dayOfWeek == DayOfWeekEnum.Sunday) 
+        //    {
+        //        Console.WriteLine($"{DayToRussian(dayOfWeek)} - выходной день");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"{DayToRussian(dayOfWeek)} - рабочий день");
+        //    }
+        //}
 
-        static string DayToRussian(DayOfWeekEnum dayOfWeek) 
-        {
-            string output = "";
-            switch (dayOfWeek)
-            {
-                case DayOfWeekEnum.Monday:
-                    output = "понедельник";
-                    break;
-                case DayOfWeekEnum.Tuesday:
-                    output = "вторник";
-                    break;
-                case DayOfWeekEnum.Wednesday:
-                    output = "среда";
-                    break;
-                case DayOfWeekEnum.Thursday:
-                    output = "четверг";
-                    break;
-                case DayOfWeekEnum.Friday:
-                    output = "пятница";
-                    break;
-                case DayOfWeekEnum.Saturday:
-                    output = "суббота";
-                    break;
-                case DayOfWeekEnum.Sunday:
-                    output = "воскресенье";
-                    break;
-            }
-            return output;
-        }
+        //static string DayToRussian(DayOfWeekEnum dayOfWeek) 
+        //{
+        //    string output = "";
+        //    switch (dayOfWeek)
+        //    {
+        //        case DayOfWeekEnum.Monday:
+        //            output = "понедельник";
+        //            break;
+        //        case DayOfWeekEnum.Tuesday:
+        //            output = "вторник";
+        //            break;
+        //        case DayOfWeekEnum.Wednesday:
+        //            output = "среда";
+        //            break;
+        //        case DayOfWeekEnum.Thursday:
+        //            output = "четверг";
+        //            break;
+        //        case DayOfWeekEnum.Friday:
+        //            output = "пятница";
+        //            break;
+        //        case DayOfWeekEnum.Saturday:
+        //            output = "суббота";
+        //            break;
+        //        case DayOfWeekEnum.Sunday:
+        //            output = "воскресенье";
+        //            break;
+        //    }
+        //    return output;
+        //}
 
-        static void PrintAllDays()
-        {
-            foreach (var item in Enum.GetValues(typeof(DayOfWeekEnum)))
-            {
-                Console.WriteLine(item);
-            }
-        }
+        //static void PrintAllDays()
+        //{
+        //    foreach (var item in Enum.GetValues(typeof(DayOfWeekEnum)))
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //}
 
-        static void PrintAllGenders()
-        {
-            foreach (var item in Enum.GetValues(typeof(GenderEnum)))
-            {
-                Console.WriteLine(item);
-            }
-        }
+        //static void PrintAllGenders()
+        //{
+        //    foreach (var item in Enum.GetValues(typeof(GenderEnum)))
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //}
     }
 }
--- lesson10_1/Program.cs
+++ lesson10_1/Program.cs
@@ -4,10 +4,14 @@ namespace lesson10_1
 {
     class Program
     {
+        public static bool isMan = true;
+        public static bool isWoman = false;
+
         static void Main(string[] args)
         {
             //клиент вводит пол и имя
-            
+            //Step2(false, "Иванова Ольга");
+            Step2(true, "Петров Иван");
         }
 
         static void Step1(bool gender, string name)
@@ -15,6 +19,22 @@ namespace lesson10_1
             if (gender == true)//муж - true, жун - false
             {
                 Console.WriteLine($"Еважаемый, {name}!");
+            }
+            else
+            {
+                Console.WriteLine($"Уважаемая, {name}!");
+            }
+
+            Console.WriteLine("Приветствуем Вас на нашем сайте!");
+
+            Console.ReadLine();
+        }
+
+        static void Step2(bool gender, string name)
+        {
+            if (gender == isMan)//муж - true, жун - false
+            {
+                Console.WriteLine($"Уважаемый, {name}!");
             }
             else
             {
